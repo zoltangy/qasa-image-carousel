5ad0b8f2ef23fd83dcfafd9c5f0a7796a97fb1a4
zoltangy <zoltangy@whatever.com>
2020-07-15 02:29:53 +0200

    Updates

---
+++ precache-manifest.a5059bd47bd2f0cd4d0db82ca6486b01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07da34dc11946cc00a9032f4f1beac48",
+    "url": "/qasa-image-carousel/index.html"
+  },
+  {
+    "revision": "89489aa895da3f63b752",
+    "url": "/qasa-image-carousel/static/js/2.99d07c37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/qasa-image-carousel/static/js/2.99d07c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa23bd8c096375f4f1dd",
+    "url": "/qasa-image-carousel/static/js/main.6cabb0c9.chunk.js"
+  },
+  {
+    "revision": "bd532497855796396af5",
+    "url": "/qasa-image-carousel/static/js/runtime-main.fbaaf27a.js"
+  }
+]);
